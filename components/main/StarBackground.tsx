8c91a37e126dc70a1fcec2ca3f3627314f4b6ff3
rohanThisSide <sablerohan125@gmail.com>
2025-06-01 03:15:35 +0530

    first commit

---
+++ components/main/StarBackground.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import { inSphere } from "maath/random";
+import * as THREE from "three";
+
+const StarBackground = (props: any) => {
+  const ref = useRef<THREE.Group>(null!);
+  const [sphere] = useState(() =>
+  inSphere(new Float32Array(15000), { radius: 1.2 })
+);
+
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
+  });
+
+  return (
+    <group ref={ref} rotation={[0, 0, Math.PI / 4]}>
+      <Points positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#ffffff"
+          size={0.002}
+          sizeAttenuation
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
+
+const StarsCanvas = () => (
+  <div className="w-full h-auto fixed inset-0 z-[20]">
+    <Canvas camera={{ position: [0, 0, 1] }}>
+      <Suspense fallback={null}>
+        <StarBackground />
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  </div>
+);
+
+export default StarsCanvas;
